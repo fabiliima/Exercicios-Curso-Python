c988b7d8b30c9831698df21a384914249f908102
fabiliima <62358790+fabiliima@users.noreply.github.com>
2020-03-23 18:11:22 -0300

    Create exercicio_1.py

---
+++ exercicio_1.py
@@ -0,0 +1,7 @@
+nota_1 = float(input('Informe a primeira nota: '))
+nota_2 = float(input('Informe a segunda nota: '))
+
+if nota_1 and nota_2 > 0 < 10:
+    print(f'A média é: {(nota_1 + nota_2) / 2:.2f}')
+else:
+    print('A nota deve ser entre 0 e 10.')]
