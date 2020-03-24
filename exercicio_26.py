47dcbb6ba5115273c089f8c6fb04a4709cc21252
fabiliima <62358790+fabiliima@users.noreply.github.com>
2020-03-24 15:06:34 -0300

    Update exercicio_26.py

--- exercicio_26.py
+++ exercicio_26.py
@@ -2,12 +2,15 @@ def media(nota1, nota2, nota3, letra):
     nota1 = float(input('Informe a primeira nota: '))
     nota2 = float(input('Informe a segunda nota: '))
     nota3 = float(input('Informe a terceira nota: '))
-    letra = (input('Digite "A" para calcular a média aritmética. \nDigite "B" para calcular a média ponderada. '
+    letra = (input('Digite "A" para calcular a média aritmética. \nDigite "P" para calcular a média ponderada. '
                    '\nDigite '
                    'sua opção: '))
     if letra.upper() == "A":
-        return (nota1 + nota2 + nota3) / 3
-    return (nota1 * 5 + nota2 * 3 + nota3 * 2) / 10
+        return f'O cálculo da média é: {nota1 + nota2 + nota3 / 3:.2f}'
+    elif letra.upper() == "P":
+        return f'O cálcula média é: {nota1 * 5 + nota2 * 3 + nota3 * 2 / 10:.2f}'
+    else:
+        "Letra não corresponde as opções."
 
 
-print(f'O cálculo da média é: {(media("nota1", "nota2", "nota3", "letra")):.2f}')
+print(media("nota1", "nota2", "nota3", "letra"))
