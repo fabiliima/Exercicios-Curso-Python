b2d2c52f5b018431755b0f2f5d3610045d145e6a
fabiliima <62358790+fabiliima@users.noreply.github.com>
2020-03-26 14:27:28 -0300

    Update exercicio_11.py

--- exercicio_11.py
+++ exercicio_11.py
@@ -1,14 +1,11 @@
-base = float(input('Informe a base do triâgulo: '))
-altura = float(input('Informe a altura do triâgulo: '))
+while True:
 
-while base <= 0:
     print('Informe valores positivos.')
     base = float(input('Informe a base do triâgulo: '))
     altura = float(input('Informe a altura do triâgulo: '))
 
-while altura <= 0:
-    print('Informe valores positivos.')
-    base = float(input('Informe a base do triâgulo: '))
-    altura = float(input('Informe a altura do triâgulo: '))
+    if base and altura > 0:
+        break
 
 print(f'A área do triângulo é: {(base * altura) / 2}')
+
