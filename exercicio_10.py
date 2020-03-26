470869c5cfd874bc79977539abb5685610266dba
fabiliima <62358790+fabiliima@users.noreply.github.com>
2020-03-26 14:22:52 -0300

    Update exercicio_10.py

--- exercicio_10.py
+++ exercicio_10.py
@@ -1,17 +1,24 @@
 altura = float(input('Qual a sua altura? '))
 peso = float(input('Qual o seu peso? '))
-
+grau_imc = ''
 imc = peso / (altura ** 2)
 
-if imc <= 18.50:
-    print(f'IMC: {imc:.2f}: Abaixo do peso')
-elif imc >= 18.60 and imc <= 24.99:
-    print(f'IMC: {imc:.2f}: Saudável')
-elif imc >= 25 and imc <= 29.99:
-    print(f'IMC: {imc:.2f}: Peso em excesso')
-elif imc >= 30 and imc <= 34.99:
-    print(f'IMC: {imc:.2f}: Obesidade Grau I')
+if imc >= 40:
+    grau_imc = 'Obesidade Graus III (Mórbida)'
+
 elif imc >= 35 and imc <= 39.99:
-    print(f'IMC: {imc:.2f}: Obesidade Grau II (Severa)')
-elif imc >= 40:
-    print(f'IMC: {imc:.2f}: Obesidade Graus III (Mórbida)')
+    grau_imc = 'Obesidade Grau II (Severa)'
+
+elif imc >= 30 and imc <= 34.99:
+    grau_imc = 'Obesidade Grau I'
+
+elif imc >= 25 and imc <= 29.99:
+    grau_imc = 'Peso em excesso'
+
+elif imc >= 18.60 and imc <= 24.99:
+    grau_imc = 'Saudável'
+
+else:
+    grau_imc = 'Abaixo do peso'
+
+print(f'IMC: {imc:.2f} Grau IMC: {grau_imc}')
