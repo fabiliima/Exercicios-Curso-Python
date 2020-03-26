4bcf731bb82dd3a1145e909e17bd85dbe5585f40
fabiliima <62358790+fabiliima@users.noreply.github.com>
2020-03-26 12:56:02 -0300

    Update exercicio_02.py

--- exercicio_02.py
+++ exercicio_02.py
@@ -1,7 +1,7 @@
 dia_da_semana = ('domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado')
-dia = int(input('Informe um número de 1 á 7: '))
+dia = int(input('Informe um número entre 1 e 7: '))
 
-if dia >= 1 and dia <=7:
+if dia >= 1 and dia <= 7:
     print(f' Hoje é {dia_da_semana[dia - 1]}.')  
 else:
     print('Número inválido.')
