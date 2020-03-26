cb606032dd24df77ef841baaafb24432f69e71c9
fabiliima <62358790+fabiliima@users.noreply.github.com>
2020-03-26 13:22:55 -0300

    Update exercicio_05.py

--- exercicio_05.py
+++ exercicio_05.py
@@ -1,8 +1,7 @@
 ano = int(input('Informe o ano: '))
 
-if ano % 400 == 0:
+if ano % 400 == 0 or ano % 4 == 0 and ano % 100 != 0:
     print('Ano bissexto!')
-elif ano % 4 == 0 and ano % 100 != 0:
-    print('Ano bissexto!')
+
 else:
     print('Ano não bissexto!')
