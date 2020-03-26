f48cb099d9e12040a1e2f2710eb3c391e9e32a4b
fabiliima <62358790+fabiliima@users.noreply.github.com>
2020-03-26 13:29:45 -0300

    Update exercicio_06.py

--- exercicio_06.py
+++ exercicio_06.py
@@ -1,14 +1,19 @@
 idade = int(input('Informe a idade: '))
 
-if idade >= 5 and idade <=7:
-    print('Infantil A')
-elif idade >= 8 and idade <= 10:
-    print('Infantil B')
-elif idade >= 11 and idade <= 13:
-    print('Juvenil A')
+if idade >= 18:
+    print('Sênior')
+
 elif idade >= 14 and idade <= 17:
     print('Juvenil B')
-elif idade >= 18:
-    print('Sênior')
+
+elif idade >= 11 and idade <= 13:
+    print('Juvenil A')
+
+elif idade >= 8 and idade <= 10:
+    print('Infantil B')
+
+elif idade >= 5 and idade <=7:
+    print('Infantil A')
+
 else:
     print('Idade incompatível com a natação.')
