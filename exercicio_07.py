96bbd82aa624ab69b7d10936fedc605f342d34e8
fabiliima <62358790+fabiliima@users.noreply.github.com>
2020-03-26 13:34:48 -0300

    Update exercicio_07.py

--- exercicio_07.py
+++ exercicio_07.py
@@ -1,22 +1,28 @@
 preco_novo = 0
 preco_antigo = float(input('Informe o preço antigo do produto: R$ '))
 
-if preco_antigo < 50:
-    preco_novo = preco_antigo * 1.05
-elif preco_antigo >= 50 and preco_antigo <= 100:
-    preco_novo = preco_antigo * 1.10
-elif preco_antigo > 100:
+if preco_antigo > 100:
     preco_novo = preco_antigo * 1.15
 
-if preco_novo < 80:
+elif preco_antigo >= 50 and preco_antigo <= 100:
+    preco_novo = preco_antigo * 1.10
+
+else:
+    preco_novo = preco_antigo * 1.05
+    
+    
+if preco_novo > 200:
     print(f'{preco_novo:.2f}')
-    print('Barato')
-elif preco_novo >= 80 and preco_novo <= 120:
-    print(f'{preco_novo:.2f}')
-    print('Normal')
+    print('Muito Caro')
+
 elif preco_novo > 120 and preco_novo <= 200:
     print(f'{preco_novo:.2f}')
     print('Caro')
-elif preco_novo > 200:
+
+elif preco_novo >= 80 and preco_novo <= 120:
     print(f'{preco_novo:.2f}')
-    print('Muito Caro')
+    print('Normal')
+
+else:
+    print(f'{preco_novo:.2f}')
+    print('Barato')
