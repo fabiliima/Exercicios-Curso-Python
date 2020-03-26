c68ded034e54ac730a94198b5f15676d584c397e
fabiliima <62358790+fabiliima@users.noreply.github.com>
2020-03-26 14:07:27 -0300

    Update exercicio_08.py

--- exercicio_08.py
+++ exercicio_08.py
@@ -1,78 +1,38 @@
+bonus = 0
+salario_reajuste = 0
+
 salario = float(input('Informe seu salário atual: '))
 tempo = int(input('Quantos anos você trabalha nesta empresa? '))
 
-if salario <= 500:
-    salario_reajuste = salario * 1.25
-    if tempo < 1:
-        bonus = 0
-        print('Funcionário não tem direito ao bônus')
-    if tempo >= 1  and tempo <= 3:
-        bonus = 100
-    if tempo >= 4 and tempo <= 6:
-        bonus = 200
-    if tempo >= 7 and tempo <= 10:
-        bonus = 300
-    if tempo > 10:
-        bonus = 500
-    print(f'O novo salário (bônus + reajuste) é: R$ {salario_reajuste + bonus:.2f}')
+if tempo > 10:
+    bonus = 500
 
-elif salario <= 1000:
-    salario_reajuste = salario * 1.20
-    if tempo < 1:
-        bonus = 0
-        print('Funcionário não tem direito ao bônus')
-    if tempo >= 1  and tempo <= 3:
-        bonus = 100
-    if tempo >= 4 and tempo <= 6:
-        bonus = 200
-    if tempo >= 7 and tempo <= 10:
-        bonus = 300
-    if tempo > 10:
-        bonus = 500
-    print(f'O novo salário (bônus + reajuste) é: R$ {salario_reajuste + bonus:.2f}')
+elif tempo >= 7 and tempo <= 10:
+    bonus = 300
 
-elif salario <= 1500:
-    salario_reajuste = salario * 1.15
-    if tempo < 1:
-        bonus = 0
-        print('Funcionário não tem direito ao bônus')
-    if tempo >= 1  and tempo <= 3:
-        bonus = 100
-    if tempo >= 4 and tempo <= 6:
-        bonus = 200
-    if tempo >= 7 and tempo <= 10:
-        bonus = 300
-    if tempo > 10:
-        bonus = 500
-    print(f"O novo salário (bônus + reajuste) é: R$ {salario_reajuste + bonus:.2f}")
+elif tempo >= 4 and tempo <= 6:
+    bonus = 200
+
+elif tempo >= 1 and tempo <= 3:
+    bonus = 100
+
+else:
+    print('Funcionário não tem direito ao bônus.')
+
+if salario > 2000:
+    salario_reajuste = salario
+    print('Funcionário não tem direito a reajuste de salário.')
 
 elif salario <= 2000:
     salario_reajuste = salario * 1.10
-    if tempo < 1:
-        bonus = 0
-        print('Funcionário não tem direito ao bônus')
-    if tempo >= 1  and tempo <= 3:
-        bonus = 100
-    if tempo >= 4 and tempo <= 6:
-        bonus = 200
-    if tempo >= 7 and tempo <= 10:
-        bonus = 300
-    if tempo > 10:
-        bonus = 500
-    print(f'O novo salário (bônus + reajuste) é: R$ {salario_reajuste + bonus:.2f}')
 
-elif salario > 2000:
-    salario_reajuste = salario
-    print('Funcionário não tem direito a reajuste.')
-    if tempo < 1:
-        bonus = 0
-        print('Funcionário não tem direito ao bônus')
-    if tempo >= 1  and tempo <= 3:
-        bonus = 100
-    if tempo >= 4 and tempo <= 6:
-        bonus = 200
-    if tempo >= 7 and tempo <= 10:
-        bonus = 300
-    if tempo > 10:
-        bonus = 500
-    print(f'O novo salário (bônus + reajuste) é: R$ {salario_reajuste + bonus:.2f}')
+elif salario <= 1500:
+    salario_reajuste = salario * 1.15
+
+elif salario <= 1000:
+    salario_reajuste = salario * 1.20
+
+elif salario <= 500:
+    salario_reajuste = salario * 1.25
+
+print(f'O novo salário (se aplicável bônus + reajuste) é: R$ {salario_reajuste + bonus:.2f}')
